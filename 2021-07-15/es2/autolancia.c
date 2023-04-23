172f06b40ceb48f6ea483335c3d2d40c3f7ca038
AlleNeri <aleneri2002@gmail.com>
2023-04-23 20:34:30 +0200

    Soluzione es2 2021-07-15, non funzionante

--- 2021-07-15/es2/autolancia.c
+++ 2021-07-15/es2/autolancia.c
@@ -14,6 +14,7 @@
 #include <unistd.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 
 //lib directory absolute path
 #define libDir "/public/"
@@ -27,17 +28,25 @@
 void execSharedLib(char *, int, char **);
 
 /**
- * Is executable or shared lib.
- * @param name the file name.
- * @return 0 if executable, 1 if shared lib.
+ * Try to execute the file in a son process.
+ * @param path path to the file.
+ * @param argv argv for the son.
+ * @return -1 if the execution went wrong.
  */
-short exeOrShared(char *);
+int tryToExec(char *, char **);
 
 int main(int argc, char ** argv) {
 	//argc error
 	if(argc<2) err(EXIT_FAILURE, "more arguments needed");
 	char * file=argv[1];
-	exeOrShared(file);
+	//create the file absolute path
+	char * filePath=malloc(strlen(libDir)+strlen(file)+1);
+	filePath=strcpy(filePath, libDir);
+	filePath=strcat(filePath, file);
+	//try to execute the file
+	if(tryToExec(file, argv)) exit(EXIT_SUCCESS);
+	//execute the shared lib
+	else execSharedLib(file, argc, argv);
 	exit(EXIT_SUCCESS);
 }
 
@@ -60,6 +69,16 @@ void execSharedLib(char * lib, int argc, char ** argv) {
 	int res=libMain(argc, argv);
 }
 
-short exeOrShared(char * name) {
-	//how to recognize an executable and a shared lib?
+int tryToExec(char * path, char ** argv) {
+	//creating the son
+	pid_t pId=fork();
+	//son case
+	if(pId==0) return execv(path, argv);
+	//dad case
+	else {
+		int status=0;
+		waitpid(pId, &status, 0);
+		printf("status: %d, %d\n", status, WIFEXITED(status));
+		return WIFEXITED(status);
+	}
 }
--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Esercizi di preparazione alla prova pratica dell'esame di sistemi operativi
 - `dlopen`: apertura di una shared lib, ritorna un handler. Corollario: `dlsym`(passato l'handler e un simbolo ne ritorna la funzione associata) e `dlerror`(ritorna un errore o NULL). [`/2021-07-15/es1/lancia.c`, `/2021-07-15/es2/autolancia.c`]
 
 # Domande
-- Come riconoscere un file eseguibile da una libreria shared? [`/2021-07-15/es2/autolancia.c`]
+- Come riconoscere un file eseguibile da una libreria shared? [`/2021-07-15/es2/autolancia.c`] [Soluzione](https://github.com/cosimopp/prove-pratiche-so/blob/main/2021.07.15/ex1.c): `exec()`: ritorna solo se è avvenuto un errore; se il parametro sara una libreria shared allora si verificherà un errore. Anche se a me non funzione.
